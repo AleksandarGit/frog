11663767213c1a8f342ce92483153b92c60fad53
Aleksandar <saledj07@gmail.com>
2019-01-21 14:15:16 +0100

    added validation rules to activity field

--- src/aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -57,7 +57,14 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
-            
+            if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
+            { 
+
+                ModelState.AddModelError("Duration", "The Duration field value must be grater than '0'.");
+
+            }
+
+
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
